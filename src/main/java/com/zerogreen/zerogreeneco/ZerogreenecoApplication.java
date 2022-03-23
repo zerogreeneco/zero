8b6beb344026ef1832302fd8f36b265029b1a1d9
iamgood147@gmail.com <iamgood147@gmail.com>
2022-03-23 13:37:56 +0900

    first commit

---
+++ src/main/java/com/zerogreen/zerogreeneco/ZerogreenecoApplication.java
@@ -0,0 +1,13 @@
+package com.zerogreen.zerogreeneco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerogreenecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerogreenecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerogreen/zerogreeneco/ZerogreenecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerogreen.zerogreeneco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerogreenecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
