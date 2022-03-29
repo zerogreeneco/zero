75538ec76925ffae7bb1c237d0f51b83a59d82b2
dorot <dorothyyyj@naver.com>
2022-03-30 00:59:03 +0900

    webSocket CORS

--- src/main/java/com/zerogreen/zerogreeneco/webSocket/configuration/WebSocketConfig.java
+++ src/main/java/com/zerogreen/zerogreeneco/webSocket/configuration/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.zerogreen.zerogreeneco.webSocket.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,10 +10,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin(origins = "http://www.zerogreen.ga")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://www.zerogreen.ga/").withSockJS();
+        registry.addEndpoint("/chat").withSockJS();
 
     }
 
