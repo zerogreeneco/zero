94f8a4b8faa9c376bf209f0570c82fd4145416a8
dorot <dorothyyyj@naver.com>
2023-04-19 14:21:43 +0900

    20230419 deploy 11

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,10 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=zero
 
 echo "> Build 파일 복사"
-
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl zero | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -22,17 +20,13 @@ else
 fi
 
 echo "> 새 어플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
-
 echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 nohup java -jar \
   -Dspring.config.location=/home/ec2-user/app/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
--- src/main/java/com/zerogreen/zerogreeneco/ZerogreenecoApplication.java
+++ src/main/java/com/zerogreen/zerogreeneco/ZerogreenecoApplication.java
@@ -2,14 +2,20 @@ package com.zerogreen.zerogreeneco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ZerogreenecoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ZerogreenecoApplication.class, args);
+		SpringApplication application = new SpringApplication(ZerogreenecoApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+
 	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(ZerogreenecoApplication.class, args);
+//	}
 
 }
