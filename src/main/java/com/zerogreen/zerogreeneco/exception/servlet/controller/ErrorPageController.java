5fe80599a6182fb7ddbb29e651a1587fc0dc0658
dorot <dorothyyyj@naver.com>
2023-04-20 13:30:42 +0900

    202304120 6

--- src/main/java/com/zerogreen/zerogreeneco/exception/servlet/controller/ErrorPageController.java
+++ src/main/java/com/zerogreen/zerogreeneco/exception/servlet/controller/ErrorPageController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class ErrorPageController {
 
-    @RequestMapping("error-page/404")
+    @RequestMapping("/error-page/404")
     public String erroPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("ERROR PAGE 404");
         return "error-page/404";
     }
 
-    @RequestMapping("error-page/500")
+    @RequestMapping("/error-page/500")
     public String erroPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("ERROR PAGE 500");
         return "error-page/500";
