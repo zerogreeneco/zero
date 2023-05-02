cde543ca207a6bbc7b236fe98bb5d10a85a8743e
dorot <dorothyyyj@naver.com>
2023-05-02 15:07:53 +0900

    20230502 12 js 파일 테스트 중

--- src/main/resources/static/bootstrap/js/map.js
+++ src/main/resources/static/bootstrap/js/map.js
@@ -98,7 +98,7 @@ $(function(){
             var address = mapAddress[j].value;
             var type = mapType[j].value;
             var shopName = document.getElementsByClassName("nn-name")[j];
-            console.log(shopName);
+            console.log("shop name "+shopName);
 
             var geocoder = new kakao.maps.services.Geocoder();
             // 마커이미지의 주소입니다
@@ -112,14 +112,12 @@ $(function(){
                     var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
                     var message = result[0].y + ', ' + result[0].x;
-                    console.log(j+"    2");
 
                 if (navigator.geolocation) {
                     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
                     navigator.geolocation.getCurrentPosition(function(position) {
                         var lat = position.coords.latitude, // 위도
                         lon = position.coords.longitude; // 경도
-                    console.log(j+"    3");
 
                         // 지도에 표시할 선을 생성합니다
                         var polyline = new kakao.maps.Polyline({
@@ -132,13 +130,19 @@ $(function(){
                     console.log(shopLeng);
 //                    var kkmmkkmm = document.getElementsByClassName("kkmmkkmm")[0];
 //                    console.log(kkmmkkmm);
+                    console.log("샵네임 "+shopName);
                     shopName.innerHTML = shopLeng;
-                    console.log(j+"    4");
                     });
                 }
+                                    var imageSrc = "/bootstrap/images/map/here.png", // 마커이미지의 주소입니다
+                                        imageSize = new kakao.maps.Size(10, 10), // 마커이미지의 크기입니다
+                                        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                                    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+                                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+
                     // 결과값으로 받은 위치를 마커로 표시합니다
                     var marker = new kakao.maps.Marker({
-                        image: imageSrc,
+                        image: markerImage,
                         position: coords
                     });
                 marker.setMap(map);
