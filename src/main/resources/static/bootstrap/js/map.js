ced580c2f7febd0437a5952fa628e0d33ec9fafa
dorot <dorothyyyj@naver.com>
2023-05-02 15:39:07 +0900

    20230502 16 js 파일 테스트 중

--- src/main/resources/static/bootstrap/js/map.js
+++ src/main/resources/static/bootstrap/js/map.js
@@ -134,16 +134,9 @@ $(function(){
                     shopName.innerHTML = shopLeng;
                     });
                 }
-                    var imageSrc = "/bootstrap/images/map/here.png", // 마커이미지의 주소입니다
-                        imageSize = new kakao.maps.Size(10, 10), // 마커이미지의 크기입니다
-                        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-
                     // 결과값으로 받은 위치를 마커로 표시합니다
-                    let marker = new kakao.maps.Marker({
-
-                        image: markerImage,
+                    var marker = new kakao.maps.Marker({
+                        map: map,
                         position: coords
                     });
                 marker.setMap(map);
