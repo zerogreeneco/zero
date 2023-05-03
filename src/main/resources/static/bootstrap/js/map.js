1732aa2aafa6f8f0493220ea4688bec5d7052cc0
dorot <dorothyyyj@naver.com>
2023-05-03 14:42:56 +0900

    20230503 6 map 가게거리 km innerHTML

--- src/main/resources/static/bootstrap/js/map.js
+++ src/main/resources/static/bootstrap/js/map.js
@@ -101,7 +101,9 @@ $(function(){
             console.log("mapType"+type);
 //            var shopName = document.getElementById("nn-name")[j];
 //            shopName.innerHTML = "<p>dfdfdf</p>";
-            document.getElementById('nn-name')[j].innerHTML = '<p>dfdfdf</p>';
+            document.getElementById('nn-name')[1].innerHTML = '<p>1111111</p>';
+            document.getElementById('nn-name')[2].innerHTML = '<p>222222</p>';
+            document.getElementById('nn-name')[3].innerHTML = '<p>333333</p>';
 
             var geocoder = new kakao.maps.services.Geocoder();
             // 마커이미지의 주소입니다
